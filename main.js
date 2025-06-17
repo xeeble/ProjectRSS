cf51d29e444508150b80b1e0c6c78c7846f08291
xeeble <49564459+xeeble@users.noreply.github.com>
2025-06-17 14:33:50 +0100

    final changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
+require('update-electron-app')()
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
